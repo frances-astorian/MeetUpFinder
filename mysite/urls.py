4bf9b9a7ac14db2da9d5493a3550ae514b7eb199
parthkasat-uva <70598129+parthkasat-uva@users.noreply.github.com>
2020-09-17 00:39:46 -0400

    Update urls.py
    remove sprint1 from path

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('sprint1/', include('sprint1.urls')),
+    path('', include('sprint1.urls')),
     path('admin/', admin.site.urls),
 ]
