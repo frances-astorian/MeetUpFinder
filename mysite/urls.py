a3ec1b5212d4eca17efe8df8609abef6900b9388
Frances Astorian <fla9fk@virginia.edu>
2020-10-07 18:43:25 -0400

    added logout button

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,5 +21,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('', TemplateView.as_view(template_name='social_app/index.html')),
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls'))
+    path('accounts/', include('allauth.urls')),
 ]
