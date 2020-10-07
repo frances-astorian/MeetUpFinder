ca1011029d1b75fdcfa8f7a6862757c7594bfdbd
Frances Astorian <fla9fk@virginia.edu>
2020-10-07 17:56:29 -0400

    updated tests.py

--- social_app/test.py
+++
@@ -1,5 +0,0 @@
-from django.test import TestCase
-
-class PracticeTest(TestCase):
-    def testTest(self):
-        self.assertTrue(True)
--- social_app/tests.py
+++ social_app/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
 # Create your tests here.
+class PracticeTest(TestCase):
+    def testTest(self):
+        self.assertTrue(True)
