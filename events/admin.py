7a2a7a4f4ae35191afadcfa90f8871c30bcd1c80
ParthKasat <parthkasat@gmail.com>
2020-10-20 02:39:43 -0400

    Update admin.py

--- events/admin.py
+++ events/admin.py
@@ -1,5 +1,22 @@
+import json
 from django.contrib import admin
-
+from django_google_maps import widgets as map_widgets
+from django_google_maps import fields as map_fields
 from .models import Event
+
+
+class EventAdmin(admin.ModelAdmin): 
+    formfield_overrides = {
+        map_fields.AddressField: {
+          'widget': map_widgets.GoogleMapsAddressWidget(attrs={'data-map-type': 'roadmap','data-autocomplete-options': json.dumps({ 'types': ['geocode','establishment'], 'componentRestrictions': {
+                  'country': 'us'
+              }
+          })}
+          )
+      }
+    }
+    list_display = ('title_text', 'location_text','time_text','category_text','description_text','address','geolocation')
+
+    
 # Register your models here.
-admin.site.register(Event)
+admin.site.register(Event,EventAdmin)
