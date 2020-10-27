c27a1b70cff84084d52c50737a98a827d38c6dc4
ParthKasat <parthkasat@gmail.com>
2020-10-27 02:39:30 -0400

    Update forms.py

--- events/forms.py
+++ events/forms.py
@@ -11,14 +11,19 @@ from .models import Event
 from django_google_maps import widgets as map_widgets
 from django_google_maps import fields as map_fields
 import json
-
+from datetime import date as currDate
 
 class EventForm(forms.ModelForm):
+    def clean_date(self):
+            date=self.cleaned_data['date']
+            if (date<currDate.today()):
+                raise forms.ValidationError("Check the time and date to make sure event is not in the past!")
+            return date
     class Meta:
         model=Event
         #fields="__all__"
-        #exclude=['geolocation']
         fields = ["title_text", "location_text", "time", "date","category_text","description_text","address"]
+        
         widgets = {
             'address':map_widgets.GoogleMapsAddressWidget(attrs={'data-map-type': 'roadmap','data-autocomplete-options': json.dumps({ 'types': ['geocode','establishment'], 'componentRestrictions': {
                   'country': 'us'
@@ -26,3 +31,4 @@ class EventForm(forms.ModelForm):
           })}
           )
         }
+       
