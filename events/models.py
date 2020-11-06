c65c3e9b2245676aad59b4a96eab9b6db6c519ba
ParthKasat <parthkasat@gmail.com>
2020-11-05 20:58:54 -0500

    Update models.py

--- events/models.py
+++ events/models.py
@@ -53,9 +53,19 @@ class Profile(models.Model):
     user=models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     bio = models.TextField()
     categories = models.ManyToManyField(Category)
+    friends = models.ManyToManyField(User, related_name='friends', blank = True)
     #profile_pic=models.ImageField(null=True, blank=True, upload_to='images/profile/')
     location = models.CharField("Location", null=True,max_length=100)
+    updated=models.DateTimeField(auto_now=True)
+    created=models.DateTimeField(auto_now_add=True)
     age = models.IntegerField(null=True)
+    
+    def get_friends(self):
+        return self.friends.all()
+    
+    def get_friends_no(self):
+        return self.friends.all().count()
+    
     def __str__(self):
         return str(self.user)
 
@@ -67,3 +77,40 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
+    
+
+STATUS_CHOICES=(
+    ('send','send'),
+    ('accepted','accepted'),
+    ('rejected','rejected'),
+    ('deleted','deleted'),
+)
+
+class Relationship(models.Model):
+    sender=models.ForeignKey(Profile, on_delete=models.CASCADE, related_name= 'sender')
+    receiver=models.ForeignKey(Profile, on_delete=models.CASCADE, related_name= 'receiver')
+    status=models.CharField(max_length=8,choices=STATUS_CHOICES)
+    updated=models.DateTimeField(auto_now=True)
+    created=models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"{self.sender}-{self.receiver}-{self.status}"
+    
+@receiver(post_save, sender=Relationship)
+def post_save_add_to_friends(sender, instance, created, **kwargs):
+    sender_=instance.sender
+    receiver_=instance.receiver
+    if instance.status=='accepted':
+        sender_.friends.add(receiver_.user)
+        receiver_.friends.add(sender_.user)
+        sender_.save()
+        receiver_.save()
+
+@receiver(post_save, sender=Relationship)
+def post_save_delete_to_friends(sender, created, instance, **kwargs):
+    sender_=instance.sender
+    receiver_=instance.receiver
+    if instance.status=='deleted':
+        sender_.friends.remove(receiver_.user)
+        receiver_.friends.remove(sender_.user)
+        sender_.save()
+        receiver_.save()
