656cf85d0e8b76ac5248ade58459dfb867993c3d
ParthKasat <parthkasat@gmail.com>
2020-11-06 02:00:35 -0500

    Update forms.py

--- social_app/forms.py
+++ social_app/forms.py
@@ -21,11 +21,16 @@ class ProfileForm(forms.ModelForm):
     bio=forms.CharField(max_length=500, widget=forms.Textarea(attrs={'class':'form-control'}))
     age=forms.IntegerField(widget=forms.NumberInput(attrs={'class':'form-control'}))
     location=forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class':'form-control'}))
-    categories = forms.ModelMultipleChoiceField(queryset=Category.objects.all(), widget=forms.CheckboxSelectMultiple)
+    categories = forms.ModelMultipleChoiceField(queryset=Category.objects.all(), required=True, widget=forms.CheckboxSelectMultiple)
     # friends = forms.ModelMultipleChoiceField(choices=Profile.get_friends(User), widget=forms.CheckboxSelectMultiple)
+    # need to edit form so that it only displays current friends and not all
     class Meta:
         model=Profile
         fields = ('bio', 'age', 'location', 'categories','friends')
+    def __init__(self, *args, **kwargs):
+        super(ProfileForm, self).__init__(*args, **kwargs)
+        # add custom error messages
+        self.fields['friends'].error_messages['required'] = 'Control Click to deselect existing friends'
     """def save(self, commit=True):
         instance = forms.ModelForm.save(self)
         instance.category_set.clear()
